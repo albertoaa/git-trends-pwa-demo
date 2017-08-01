b0a57b91526a50fcd6e79537e475be0928a213e2
Alberto Aragon <alberto@alturasoluciones.com>
2017-08-01 16:13:26 -0500

    Date conversion and Github's trends fetch and update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,38 @@
 (function () {
+
+  Date.prototype.yyyymmdd = function () {
+    let mm = this.getMonth() + 1;
+    let dd = this.getDate();
+
+    return [this.getFullYear(),
+      (mm > 9 ? '' : '0') + mm,
+      (dd > 9 ? '' : '0') + dd,
+    ].join('-');
+  };
+
+  const dates = {
+    startDate: function() {
+      const startDate = new Date();
+      startDate.setDate(startDate.getDate() - 7);
+      return startDate.yyyymmdd();
+    },
+    endDate: function() {
+      const endDate = new Date();
+      return endDate.yyyymmdd();
+    }
+  }
+
   const app = {
     apiURL: `https://api.github.com/search/repositories?q=created%3A%22${dates.startDate()}+..+${dates.endDate()}%22%20language%3Ajavascript&sort=stars&order=desc`
   }
 
+  app.updateTrends = function (trends) {
+    const trendsRow = document.querySelector('.trends');
+    for (let i = 0; i < trends.lenght; i++) {
+      const trend = trends[i];
+      trendsRow.appendChild(app.createCard(trend));
+    }
+  }
   app.getTrends = function () {
     fetch(app.apiURL)
       .then(response => response.json())
@@ -17,7 +47,7 @@
   document.addEventListener('DOMContentLoaded', function () {
     app.getTrends()
   })
-  
+
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('/service-worker.js')
